32b93064f0561557b640a22914f110af0e2ec7bb
MrkDvdL <100331088+MrkDvdL@users.noreply.github.com>
2022-03-02 12:31:20 +0800

    to clean up

---
+++ searchbar/src/component/Autocomplete.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+export default function Autocomplete({ suggestions }) {
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onChange = (e) => {
+    const userInput = e.target.value;
+
+    const unLinked = suggestions.filter(
+      (suggestion) =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    setInput(e.target.value);
+    setFilteredSuggestions(unLinked);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(true);
+  };
+
+  const onClick = (e) => {
+    setFilteredSuggestions([]);
+    setInput(e.target.innerText);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(false);
+  };
+
+  const onKeyDown = (e) => {};
+
+  const SuggestionsListComponent = () => {
+    return filteredSuggestions.length ? (
+      <ul className="suggestions">
+        {filteredSuggestions.map((suggestion, index) => {
+          let className;
+          if (index === activeSuggestionIndex) {
+            className = "suggestion-active";
+          }
+          return (
+            <li className={className} key={suggestion} onClick={onClick}>
+              {suggestion}
+            </li>
+          );
+        })}
+      </ul>
+    ) : (
+      <div class="no-suggestions">
+        <em>Not Found</em>
+      </div>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        onChange={onChange}
+        value={input}
+        onKeyDown={onKeyDown}
+        onClick={() => setShowSuggestions(!showSuggestions)}
+      />
+      {showSuggestions && input && <SuggestionsListComponent />}
+    </React.Fragment>
+  );
+}
